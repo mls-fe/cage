2bbfd62f133e4f443a66a8bf74a534b25a3096ee
sunnylost <sunnylost@gmail.com>
2016-04-19 00:26:59 +0800

    remove keyMirro dependent.

--- src/key.js
+++ src/key.js
@@ -1,15 +1,23 @@
-let KeyMirror = require( 'keymirror' )
+let KeyMirror = obj => {
+    var ret = {}
+
+    Object.keys( obj ).forEach( key => {
+        ret[ key ] = key
+    } )
+
+    return ret
+}
 
 module.exports = KeyMirror( {
-    profile: null,
-    current_path: null,
-    workspace_list: null,
-    username: null,
-    password: null,
-    domains: null,
-    domainsSize: null,
-    ip: null,
-    mac: null,
-    random: null,
-    normal: null
+    profile        : null,
+    current_path   : null,
+    workspace_list : null,
+    username       : null,
+    password       : null,
+    domains        : null,
+    domainsSize    : null,
+    ip             : null,
+    mac            : null,
+    random         : null,
+    normal         : null
 } )
