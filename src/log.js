c1218b28eccd8ae868ace14c9602d51a8c169c96
sunnylost <sunnylost@gmail.com>
2016-04-24 00:43:09 +0800

    remove Chalk

--- src/log.js
+++ src/log.js
@@ -1,12 +1,59 @@
-let Chalk  = require( 'chalk' ),
+//https://github.com/chalk/ansi-styles/blob/master/index
+let levels = [
+        {
+            name  : 'info',
+            color : 'white'
+        },
+        {
+            name  : 'debug',
+            color : 'gray'
+        },
+        {
+            name  : 'error',
+            color : 'red',
+            bold  : true
+        },
+        {
+            name  : 'warn',
+            color : 'yellow',
+            bold  : true
+        },
+        {
+            name  : 'success',
+            color : 'green'
+        }
+    ],
+    bold   = [ 1, 22 ],
     colors = {
-        info: Chalk.white,
-        debug: Chalk.gray,
-        error: Chalk.red.bold,
-        warn: Chalk.yellow.bold,
-        success: Chalk.green
+        red    : [ 31, 39 ],
+        green  : [ 32, 39 ],
+        yellow : [ 33, 39 ],
+        white  : [ 37, 39 ],
+        gray   : [ 90, 39 ]
+    },
+    edge   = '\u001b[',
+    style  = {},
+    helper = content => `${edge}${content}m`
+
+levels.forEach( level => {
+    let color  = colors[ level.color ],
+        isBold = !!level.bold
+
+    style[ level.name ] = content => {
+        let str = [ content ]
+
+        str.unshift( helper( color[ 0 ] ) )
+        str.push( helper( color[ 1 ] ) )
+
+        if ( isBold ) {
+            str.unshift( helper( bold[ 0 ] ) )
+            str.push( helper( bold[ 1 ] ) )
+        }
+
+        return str.join( '' )
     }
+} )
 
 global.log = ( content, type = 'info' ) => {
-    console.log( colors[ type ]( content ) )
+    console.log( style[ type ]( content ) )
 }
