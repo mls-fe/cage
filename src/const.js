0fafc9798ee635160e931dc5ad1dbfecfb6c2722
sunnylost <sunnylost@gmail.com>
2016-04-25 13:14:40 +0800

    little fix

--- src/const.js
+++ src/const.js
@@ -1,16 +1,13 @@
-let DIR_CONFIG = '/nest/server/config',
-    FILE_ETC   = `${DIR_CONFIG}/etc.json`,
-    FILE_SITE  = `${DIR_CONFIG}/site.json`,
-    FILE_VHOST = `${DIR_CONFIG}/virtual_host.json`
+let DIR_CONFIG = '/nest/server/config'
 
 module.exports = {
     APPS         : '/apps',
     NEST         : '/nest',
-    FILE_ETC,
-    FILE_SITE,
-    FILE_VHOST,
+    FILE_ETC     : `${DIR_CONFIG}/etc.json`,
+    FILE_SITE    : `${DIR_CONFIG}/site.json`,
+    FILE_VHOST   : `${DIR_CONFIG}/virtual_host.json`,
     FILE_SERVICE : '/nest/jserver/config/service.json',
     SITE_SUFFIX  : '.fedevot.meilishuo.com',
     URL_SERVER   : '172.18.16.20',
-    URL_PORT     : '2016'
+    URL_PORT     : '2018'
 }
