22f29581f0e28f32b3bef021b2fe708cf461fa7f
sunnylost <sunnylost@gmail.com>
2016-05-14 01:21:07 +0800

    check node version

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,6 @@
-#!/usr/bin/env node --harmony
+#!/usr/bin/env node
+if ( parseFloat( process.version.replace( 'v', '' ) ) < 4.2 ) {
+    return console.warn( 'Node 版本过低, 请升级至最新版本.' )
+}
 
 require( '../dist/index' )
