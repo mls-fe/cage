7cf52a2f2474efc7e993a7057b1e5c9230058732
sunnylost <sunnylost@gmail.com>
2016-05-14 11:48:05 +0800

    little change

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-if ( parseFloat( process.version.replace( 'v', '' ) ) < 4.2 ) {
+if ( parseFloat( process.versions.node ) < 4.2 ) {
     return console.warn( 'Node 版本过低, 请升级至最新版本.' )
 }
 
